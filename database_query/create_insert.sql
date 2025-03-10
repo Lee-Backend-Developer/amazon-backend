df70680846843dce63d886971e1f388b7b044490
lee <eotmd1016@gmail.com>
2025-03-10 13:55:14 +0900

    DB : 값 추가하였음

--- database_query/create_insert.sql
+++ database_query/create_insert.sql
@@ -5,9 +5,12 @@ insert product(name, cnt, main_image, price, description)
     value ('Amazon Basics PC에서 스트리밍, 녹음 및 팟캐스트를 위한 USB 콘덴서 마이크, 플러그 앤 플레이, 카디오이드 픽업, 360° 회전이 가능한 조절 가능한 스탠드, 14.7 x 8.6cm(5.8 x 3.4인치), 블랙',
            10, 'test.jpg', 35000, '이것은 마이크 입니다');
 
-
-insert cart(product_fk, product_cnt, member_fk)
-    value (1, 2, 1);
-
 insert category(name, product_fk)
     value('마이크', 1);
+
+insert cart(member_fk)
+    value (1);
+
+insert cart_product(product_fk, cart_fk, product_cnt)
+    value (1, 3, 1);
+
