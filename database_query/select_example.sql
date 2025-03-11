7af640f2d45c9427a5a980d832c7f341e65ec406
lee <eotmd1016@gmail.com>
2025-03-11 22:02:44 +0900

    BD : 조인쿼리 수정하였음

--- database_query/select_example.sql
+++ database_query/select_example.sql
@@ -2,8 +2,8 @@
 # 카트 ID, 회원정보, 제품명, 수량 조회
 select
     c.id as '카트ID',
-    (select name from member where member.id = c.member_fk) as '회원이름',
-    (select name from product where product.id = cp.product_fk) as '제품명',
+    (select name from member where member.id = c.member_fk_id) as '회원이름',
+    (select name from product where product.id = cp.product_fk_id) as '제품명',
     cp.product_cnt as '제품수량'
     from cart c inner join cart_product cp
-on c.id = cp.cart_fk;
+on c.id = cp.cart_fk_id;
