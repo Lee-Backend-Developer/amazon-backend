cde80b56d259c4fa90eeeafa710728ba6da24540
lee <eotmd1016@gmail.com>
2025-03-13 15:54:14 +0900

    회원 : 회원 저장소 설계

---
+++ src/main/java/com/amazon_clone/amazon/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.amazon_clone.amazon.member.repository;
+
+import com.amazon_clone.amazon.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
