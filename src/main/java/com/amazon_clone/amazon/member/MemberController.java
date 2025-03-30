519ee7d1e9b6fc31e12dc006743e29b7c746353c
lee <eotmd1016@gmail.com>
2025-03-30 17:41:36 +0900

    회원-컨트롤러 : 회원가입 메소드 POST -> PUT로 수정

--- src/main/java/com/amazon_clone/amazon/member/MemberController.java
+++ src/main/java/com/amazon_clone/amazon/member/MemberController.java
@@ -7,10 +7,7 @@ import com.amazon_clone.amazon.member.response.MemberResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,7 +16,7 @@ public class MemberController {
     private final MemberService memberService;
 
     //회원가입
-    @PostMapping("register")
+    @PutMapping("register")
     public ResponseEntity<Response> register(@RequestBody MemberRegister request) {
         Member register = memberService.register(request);
         MemberResponse memberResponse = new MemberResponse(register, HttpStatus.OK);
