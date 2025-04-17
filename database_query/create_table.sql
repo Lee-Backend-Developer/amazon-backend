ddb84b894bd042806431510077bdb954a5f48ca7
lee <eotmd1016@gmail.com>
2025-04-17 23:14:20 +0900

    SQL : 딜리버리 테이블 삭제, 주문제품 생성 다대일로 하였음

--- database_query/create_table.sql
+++ database_query/create_table.sql
@@ -13,24 +13,24 @@ create table member
 
 create table category
 (
-    id            bigint auto_increment comment '카테고리_고유번호',
-    name          varchar(255) not null comment '카테고리_이름',
+    id   bigint auto_increment comment '카테고리_고유번호',
+    name varchar(255) not null comment '카테고리_이름',
 
     constraint category_fk primary key (id)
 );
 
 create table product
 (
-    id          bigint auto_increment comment '제품_고유번호',
-    name        varchar(255) not null comment '제품이름',
-    cnt         int          not null default 0 comment '제품수량',
-    main_image  varchar(255),
-    price       int          not null default 0 comment '제품 가격',
-    description longtext comment '제품설명',
-    category_fk_id bigint not null comment '카테고리_fk',
+    id             bigint auto_increment comment '제품_고유번호',
+    name           varchar(255) not null comment '제품이름',
+    cnt            int          not null default 0 comment '제품수량',
+    main_image     varchar(255),
+    price          int          not null default 0 comment '제품 가격',
+    description    longtext comment '제품설명',
+    category_fk_id bigint       not null comment '카테고리_fk',
 
     constraint product_fk primary key (id),
-    constraint product_category_fk foreign key (category_fk_id) references category(id)
+    constraint product_category_fk foreign key (category_fk_id) references category (id)
 );
 
 create table cart
@@ -54,28 +54,27 @@ create table cart_product
     constraint cart_product_product_fk foreign key (product_fk_id) references product (id)
 );
 
-create table orders
+create table orders_number_generator
 (
-    id           bigint auto_increment comment '주문_고유번호',
-    cart_fk_id   bigint comment '카트_fk',
-    member_fk_id bigint comment '회원_fk',
-    product_cnt  int comment '제품_수량',
+    id        bigint auto_increment comment '주문_고유번호',
+    member_fk_id bigint comment '회원_고유번호_fk',
+    delivery_status  enum ('ordered','ready','delivered') comment '배송상태',
 
     constraint orders_id primary key (id),
-    constraint orders_product_fk foreign key (cart_fk_id) references product (id),
-    constraint orders_member_fk foreign key (member_fk_id) references member (id)
+    constraint orders_number_generator_member_fk foreign key (member_fk_id) references member (id)
 );
 
-create table delivery
+create table orders_product
 (
-    id           bigint auto_increment comment '배송_고유번호',
-    orders_fk_id bigint comment '주문_fk',
-    state        enum ('ordered','ready','delivered'),
+    id            bigint auto_increment comment '주문_제품_고유번호',
+    product_fk_id bigint comment '제품_fk',
+    orders_number_generator_fk_id  bigint comment '주문번호_fk',
+    product_cnt   int comment '제품_수량',
 
-    constraint delivery_id primary key (id),
-    constraint delevery_order_fk foreign key (orders_fk_id) references orders (id)
+    constraint orders_product_id primary key (id),
+    constraint orders_product_product_fk foreign key (product_fk_id) references product (id),
+    constraint orders_product_orders_fk foreign key (orders_number_generator_fk_id) references orders_number_generator (id)
 );
-
 
 create table review
 (
