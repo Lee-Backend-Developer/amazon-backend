2716453e012b86eb4c068dec932e36fc1ea3ff98
lee <eotmd1016@gmail.com>
2025-05-05 20:06:47 +0900

    web 설정 : 웹 시큐리티 설정 ㅜㅜㅜ

---
+++ src/main/java/com/amazon_clone/amazon/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.amazon_clone.amazon.config;
+
+import com.amazon_clone.amazon.member.MemberDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final MemberDetailService memberDetailService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(
+                authorize ->
+                authorize
+                        // 게스트 유저가 접근할 수 있는 URL
+                        .requestMatchers("/api/member/login", "/api/member/register")
+                        .permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        http.logout(logout -> logout.logoutUrl("/api/member/logout"));
+
+        return http.build();
+    }
+
+   /* @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(memberDetailService)
+                .and().build();
+
+
+    }*/
+
+
+}
