74dffab70d9796252ecd360c542a5b2e7c59aad6
lee <eotmd1016@gmail.com>
2025-07-02 23:34:36 +0900

    테스트 : 테스트 전용 시큐리티 파일 작성하였음

--- src/test/java/com/amazon_clone/amazon/config/TestSecurityConfig.java
+++ src/test/java/com/amazon_clone/amazon/config/TestSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
@@ -11,8 +12,25 @@ public class TestSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.formLogin(formLogin ->
+                formLogin.loginPage("/api/member/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
+        );
+
+        http.authorizeHttpRequests(
+                auth ->
+                        auth.anyRequest().permitAll());
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new NoOpPasswordEncoder();
     }
 }
