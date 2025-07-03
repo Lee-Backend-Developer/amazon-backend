ee33dea72686dc5f9deae62f5386340e68375300
lee <eotmd1016@gmail.com>
2025-07-03 21:56:44 +0900

    테스트 : 제품 테스트 클래스 생성 및 기본필드 작성 완료

---
+++ src/test/java/com/amazon_clone/amazon/product/ProductControllerAPITest.java
@@ -0,0 +1,31 @@
+package com.amazon_clone.amazon.product;
+
+import com.amazon_clone.amazon.product.domain.Product;
+import com.amazon_clone.amazon.product.request.ProductAddRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+@AutoConfigureMockMvc(addFilters = false) // 스프링 시큐리티 필터 비활성화
+@AutoConfigureRestDocs
+class ProductControllerAPITest {
+
+    @MockitoBean
+    private ProductService productService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    
+}
