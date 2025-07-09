ab0c70a3009a6ea706ff8cd69baffeb7f340a0b4
lee <eotmd1016@gmail.com>
2025-07-09 15:26:31 +0900

    삭제 : 코드 모두 삭제

--- src/main/java/com/amazon_clone/amazon/member/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.amazon_clone.amazon.member.domain;
-
-public enum Role {
-    USER, ADMIN
-}
--- src/main/java/com/amazon_clone/amazon/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.amazon_clone.amazon.product.repository;
-
-import com.amazon_clone.amazon.product.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
